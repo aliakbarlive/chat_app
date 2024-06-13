566fc6cd291320e75d54f30dcdaa9c14476ca521
Ali Akbar <115136332+aakbar2022@users.noreply.github.com>
2024-06-13 15:42:54 +0500

    setup tailwind config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: { mode: string }) => {
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      'process.env': { ...process.env, ...loadEnv(mode, process.cwd()) }
+    },
+    server: {
+      port: 3000
+    }
+  })
+}
